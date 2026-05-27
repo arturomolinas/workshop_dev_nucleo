490825aa243f0474f3eb4ebf17dd9915bb0b209b
waltergauto <61380032+waltergauto@users.noreply.github.com>
2026-05-27 00:30:25 +0000

    Add python-ready

---
+++ python-ready/app.py
@@ -0,0 +1,24 @@
+import time
+from flask import Flask, Response
+app = Flask(__name__)
+
+start_time = time.time()
+
+@app.route('/')
+def index():
+    return "App con Probes lista.\n"
+
+@app.route('/healthz')
+def healthz():
+    # Simula que la app falla después de 60 segundos de vida
+    if time.time() - start_time > 60:
+        return Response("Fallo interno", status=500)
+    return "OK", 200
+
+@app.route('/ready')
+def ready():
+    # Endpoint simple para readiness
+    return "Listo", 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
