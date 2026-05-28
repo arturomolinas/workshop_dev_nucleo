9e6589802663fcba2c7ee5cdeb10c612ec9bfe91
waltergauto <61380032+waltergauto@users.noreply.github.com>
2026-05-28 09:14:28 -0300

    Update app.py

--- python-ready/app.py
+++ python-ready/app.py
@@ -18,7 +18,7 @@ def healthz():
 @app.route('/ready')
 def ready():
     # Endpoint simple para readiness
-    return "Listo", 200
+    return "Listo v2", 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
